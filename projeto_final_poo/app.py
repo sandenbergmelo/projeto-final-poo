83b1a1c870d56e7958eb33d5a80d255e90e093f6
Sandenberg Ferreira Melo <77549672+sandenbergmelo@users.noreply.github.com>
2024-09-17 00:35:23 -0300

    Address route comes before services

--- projeto_final_poo/app.py
+++ projeto_final_poo/app.py
@@ -4,9 +4,9 @@ from projeto_final_poo.routers import address, client, schedules, services
 
 app = FastAPI()
 app.include_router(client.router)
+app.include_router(address.router)
 app.include_router(services.router)
 app.include_router(schedules.router)
-app.include_router(address.router)
 
 
 @app.get('/')
